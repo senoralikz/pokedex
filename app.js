5bb43488783eb166d033f781d2a484bc4144d461
senoralikz <alexjm825@gmail.com>
2021-09-15 04:06:28 -0400

    added sorting options and search bar

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const pokedex = $("#pokedex");
 
+let pokemon = [];
+
 const fetchPokemon = () => {
   // initialize array that will be filled with each pokemons url
   const promises = [];
@@ -16,19 +18,19 @@ const fetchPokemon = () => {
   // Using Promise.all to wait to receive all information that is requested from the pokemon url
   Promise.all(promises).then((res) => {
     // then initialize and set the properties we want to use from the response
-    const pokemon = res.map((res) => ({
+    pokemon = res.map((res) => ({
       name: res.name,
       id: res.id,
       sprite: res.sprites["front_default"],
       // type is an array so we join each string inside the type array to set it to one type property
       type: res.types.map((type) => type.type.name).join(", "),
     }));
-    displayPokemon(pokemon);
+    displayPokemon();
     console.log(pokemon);
   });
 };
 
-const displayPokemon = (pokemon) => {
+const displayPokemon = () => {
   const pokemonHtml = pokemon
     .map(
       (pokemon) =>
@@ -45,4 +47,56 @@ const displayPokemon = (pokemon) => {
   pokedex.html(pokemonHtml);
 };
 
+const sortPokemonName = () => {
+  pokemon.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+
+  displayPokemon();
+};
+
+const reversePokemonName = () => {
+  sortPokemonName();
+
+  pokemon.reverse();
+
+  displayPokemon();
+};
+
+const sortPokemonId = () => {
+  pokemon.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+
+  displayPokemon();
+};
+
+const reversePokemonId = () => {
+  sortPokemonId();
+
+  pokemon.reverse();
+
+  displayPokemon();
+};
+
+const sortingOptions = () => {
+  let selValue = $("#sortOptions").val();
+
+  if (selValue === "1") {
+    sortPokemonName();
+  } else if (selValue === "2") {
+    reversePokemonName();
+  } else if (selValue === "3") {
+    sortPokemonId();
+  } else if (selValue === "4") {
+    reversePokemonId();
+  }
+};
+
+$(document).ready(function () {
+  $("#searchPokemon").on("keyup", function () {
+    let value = $(this).val().toLowerCase();
+    $("#pokedex div").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
+
 fetchPokemon();
+$("#sortOptions").on("change", sortingOptions);
