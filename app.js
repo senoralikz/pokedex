b2a9840e0b55a075e97d0e5d644f347d232b8dcf
senoralikz <alexjm825@gmail.com>
2021-09-16 19:14:07 -0400

    added styling to pokemon type and moved loading image a little lower

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ let startingId = 1;
 let endingId = 151;
 let pokemon = [];
 
-pokedex.html(`
+const loading = `
   <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+  `;
+
+pokedex.html(loading);
 
 const fetchPokemon = () => {
   // initialize array that will be filled with each pokemons url
   const promises = [];
 
-  // get url for pokemon id up to 151
+  // get url for pokemon id
   for (let i = startingId; i <= endingId; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
 
@@ -28,27 +30,52 @@ const fetchPokemon = () => {
       id: res.id,
       sprite: res.sprites["front_default"],
       // type is an array so we join each string inside the type array to set it to one type property
-      type: res.types.map((type) => type.type.name).join(", "),
+      // type: res.types,
+      // type: res.types.map((type) => type.type.name).join(", "),
+      type: res.types.map((type) => type.type.name),
     }));
     displayPokemon();
     console.log(pokemon);
+    console.log(pokemon[5].type);
+    console.log(pokemon[5].type[1]);
   });
 };
 
 const displayPokemon = () => {
-  const pokemonHtml = pokemon
-    .map(
-      (pokemon) =>
-        `
+  let pokemonHtml;
+
+  pokemonHtml = pokemon.map(
+    (pokemon) =>
+      `
       <div class='card'>
         <p class='pokemon-id'>#${pokemon.id}</p>
         <img class='card-img' src='${pokemon.sprite}' />
         <h5 class='card-title'>${pokemon.name}</h5>
-        <p class='card-text'>Type: ${pokemon.type}</p>
+        <div class='d-flex justify-content-start'>
+          <span class='type ${pokemon.type[0]}'><span class='type-text'>${pokemon.type[0]}</span></span>
+          <span id='type2' class='type ${pokemon.type[1]}'><span class='type-text'>${pokemon.type[1]}</span></span>
+        </div>
       </div>
     `
-    )
-    .join("");
+  );
+
+  for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].type.length === 1) {
+      pokemonHtml[i] = `
+      <div class='card'>
+        <p class='pokemon-id'>#${pokemon[i].id}</p>
+        <img class='card-img' src='${pokemon[i].sprite}' />
+        <h5 class='card-title'>${pokemon[i].name}</h5>
+        <div class='d-flex justify-content-start'>
+          <span class='type ${pokemon[i].type[0]}'><span class='type-text'>${pokemon[i].type[0]}</span></span>
+        </div>
+      </div>
+    `;
+    }
+  }
+
+  // .join("");
+
   pokedex.html(pokemonHtml);
 };
 
@@ -101,81 +128,63 @@ const genSelection = () => {
     startingId = 1;
     endingId = 151;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "2") {
     startingId = 152;
     endingId = 251;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "3") {
     startingId = 252;
     endingId = 386;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "4") {
     startingId = 387;
     endingId = 493;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "5") {
     startingId = 494;
     endingId = 649;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "6") {
     startingId = 650;
     endingId = 721;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "7") {
     startingId = 722;
     endingId = 809;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "8") {
     startingId = 810;
     endingId = 898;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   } else if (genValue === "9") {
     startingId = 1;
     endingId = 898;
 
-    pokedex.html(`
-  <img class='loading' src='./loading-image/pikachu - loading.gif' alt='pikachu_loading' />
-  `);
+    pokedex.html(loading);
 
     fetchPokemon();
   }
