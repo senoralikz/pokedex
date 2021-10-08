386243e0a54b9a3e95b3d3767a9e72039cae343e
senoralikz <alexjm825@gmail.com>
2021-10-07 23:07:49 -0400

    added border radius and active class to page currently viewing, added function to close hamburger menu if clicked anywhere in the body other than the header

--- app.js
+++ app.js
@@ -2,36 +2,42 @@ import { showPokemonTab } from "./pokemon.js";
 import { showAbilitiesTab } from "./abilities.js";
 import { showMovesTab } from "./moves.js";
 
-const homePage = () => {
-  $("#searchPokemon").val("");
-  $(".landing-page").show();
-  $(".pokemon-tab").hide();
-  $(".abilities-tab").hide();
-  $(".moves-tab").hide();
-  $(".items-tab").hide();
-};
-
-$(document).ready(function () {
-  $("#searchPokemon").on("keyup", function () {
-    let value = $(this).val().toLowerCase();
-    $("#pokedex div.pokemon-card").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-    $(".ability-table-body tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-    $(".move-table-body tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
-});
-
 $(() => {
   $(".pokemon-tab").hide();
   $(".abilities-tab").hide();
   $(".moves-tab").hide();
 
-  $(".pokedex-logo").on("click", homePage);
+  $(document).ready(function () {
+    $("#searchPokemon").on("keyup", function () {
+      let value = $(this).val().toLowerCase();
+      $("#pokedex div.pokemon-card").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+      $(".ability-table-body tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+      $(".move-table-body tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+    });
+  });
+
+  $(".section-button").on("click", function () {
+    $(".section-pages").removeClass("show");
+    $(".section-button.active").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".pokedex-logo").on("click", function () {
+    $(".section-button.active").removeClass("active");
+    $(".section-pages").removeClass("show");
+    $("#searchPokemon").val("");
+    $(".landing-page").show();
+    $(".pokemon-tab").hide();
+    $(".abilities-tab").hide();
+    $(".moves-tab").hide();
+    $(".items-tab").hide();
+  });
   $(".pokemon-section").on("click", showPokemonTab);
   $(".abilities-section").on("click", showAbilitiesTab);
   $(".moves-section").on("click", showMovesTab);
@@ -39,6 +45,9 @@ $(() => {
     $(".section-pages").toggleClass("show");
   });
   $(".section-pages li").on("click", function () {
+    $(".section-pages").removeClass("show");
+  });
+  $(".poke-data").on("click", function () {
     $(".section-pages").removeClass("show");
   });
 });
